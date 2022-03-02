ee300e59db5a2c1b62ed22780f6940ca44fe6dc2
sharinfernandes <62426486+sharinfernandes@users.noreply.github.com>
2022-03-02 16:48:51 +0530

    Add files via upload

---
+++ banking_app/src/test/java/com/cts/springboot/banking/service/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.springboot.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
