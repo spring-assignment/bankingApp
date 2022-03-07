b104979d8b9bfddb86b23c5c9077a454fdfb31b4
divyapalani-1 <100764116+divyapalani-1@users.noreply.github.com>
2022-03-07 12:06:26 +0530

    Add files via upload

---
+++ banking_app/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO investment_plan VALUES(1,'PLAN-A','Can Invest in real estate
+Can invest in equity mutual funds
+Sovereign Gold Bonds');
+INSERT INTO investment_plan VALUES(2,'PLAN-B','Fixed deposit can be made for 15 years and you get assured returns over time
+Can invest in Senior Citizen Savings Scheme');
