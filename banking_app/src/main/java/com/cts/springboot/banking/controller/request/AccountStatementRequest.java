e5fc19bb088afc0451386088f52f3fa16c3d9277
divyapalani-1 <100764116+divyapalani-1@users.noreply.github.com>
2022-03-07 12:30:11 +0530

    Update AccountStatementRequest.java
    Update the class with two fields fromDate and toDate and their setter methods.

--- banking_app/src/main/java/com/cts/springboot/banking/controller/request/AccountStatementRequest.java
+++ banking_app/src/main/java/com/cts/springboot/banking/controller/request/AccountStatementRequest.java
@@ -1,11 +1,37 @@
 package com.cts.springboot.banking.controller.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
-
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Data
 public class AccountStatementRequest {
+
     private String accountNumber;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private String fromDate;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private String toDate;
+
+    public void setFromDate(String fromDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String date = fromDate;
+        LocalDate localDate = LocalDate.parse(date, formatter);         //convert String to LocalDate
+        this.fromDate = String.valueOf(localDate);
+
+    }
+
+    public void setToDate(String toDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String date = toDate;
+        LocalDate localDate = LocalDate.parse(date, formatter);
+        this.toDate = String.valueOf(localDate);                        //convert String to LocalDate
+
+    }
 }
