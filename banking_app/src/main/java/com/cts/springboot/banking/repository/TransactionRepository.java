59d1536312e4026b0b1acfcc2bafd4a515bd305f
divyapalani-1 <100764116+divyapalani-1@users.noreply.github.com>
2022-03-07 12:07:56 +0530

    Update TransactionRepository.java
    Added findByAccountNumberByDate() method to filter based on date

--- banking_app/src/main/java/com/cts/springboot/banking/repository/TransactionRepository.java
+++ banking_app/src/main/java/com/cts/springboot/banking/repository/TransactionRepository.java
@@ -2,11 +2,16 @@ package com.cts.springboot.banking.repository;
 
 import com.cts.springboot.banking.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     List<Transaction> findByAccountNumber(String accountNumber);
 
+    @Query("select a from Transaction a where a.accountNumber=:accountNumber AND a.transactionDate BETWEEN :fromDate AND :toDate")
+    List<Transaction> findByAccountNumberByDate(@Param("accountNumber") String accountNumber, @Param("fromDate") LocalDate fromDate, @Param("toDate") LocalDate toDate);
 }
