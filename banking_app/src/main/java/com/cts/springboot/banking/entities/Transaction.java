47c4f25b9a5a8ba9f759c5ed460a7ba6e559f31e
divyapalani-1 <100764116+divyapalani-1@users.noreply.github.com>
2022-03-16 19:48:02 +0530

    Update Transaction.java
    @Column annotation have been added to the fields in the transaction table.

--- banking_app/src/main/java/com/cts/springboot/banking/entities/Transaction.java
+++ banking_app/src/main/java/com/cts/springboot/banking/entities/Transaction.java
@@ -17,18 +17,23 @@ public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Long transactionId;
 
+    @Column
     private String accountNumber;
 
+    @Column
     private String toAccountNumber;
 
+    @Column
     private BigDecimal transactionAmount;
 
+    @Column
     private LocalDate transactionDate;
-
 
     public Transaction(LocalDate transactionDate) {
         this.transactionDate = transactionDate;
     }
 }
+
