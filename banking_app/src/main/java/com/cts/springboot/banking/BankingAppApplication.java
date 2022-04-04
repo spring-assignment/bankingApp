2e1368fc7dc72d1631a54712b23391317dfe0ea7
sharinfernandes <62426486+sharinfernandes@users.noreply.github.com>
2022-04-04 11:51:49 +0530

    Update BankingAppApplication.java

--- banking_app/src/main/java/com/cts/springboot/banking/BankingAppApplication.java
+++ banking_app/src/main/java/com/cts/springboot/banking/BankingAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableAsync
 public class BankingAppApplication {
 
 	public static void main(String[] args) {
